83736b8e8033036229f124fb2e969d24cefcd4b4
dharien-llanos <dharien1317@gmail.com>
2024-07-09 22:49:29 -0500

    agrege la funcionalidad de Listar los productos registrados

---
+++ 2)_listas.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int MAX_PRODUCTOS = 100;
+const int MAX_VENTAS = 100;
+
+struct Producto {
+    string nombre;
+    float precio;
+};
+
+struct Venta {
+    int idVenta;
+    string producto;
+    int cantidad;
+    float precioTotal;
+};
+
+Producto productos[MAX_PRODUCTOS];
+Venta ventas[MAX_VENTAS];
+int numProductos=0;
+int numVentas=0;
+int idVentaCounter=1;
+
+void registrarProducto() 
+{
+    if (numProductos>=MAX_PRODUCTOS)
+	 {
+        cout<<"No se pueden registrar mas productos. El limite ha sido alcanzado"<<endl;
+        return;
+    }
+
+    Producto p;
+    cout<<"Ingrese el nombre del producto: ";
+    cin.ignore();
+    getline(cin,p.nombre);
+    cout<<"Ingrese el precio del producto: ";
+    cin>>p.precio;
+
+    productos[numProductos++]=p;
+    cout<<"Producto registrado exitosamente"<<endl;
+}
+void listarProductos() {
+    cout<<"\nLista de productos:\n";
+    for (int i=0; i<numProductos; i++) 
+	{
+        cout<<"Nombre: "<< productos[i].nombre<< ", Precio: "<<productos[i].precio<<endl;
+    }
+}
+
+int main() 
+{
+    char opcion;
+    do {
+        cout<<"Menu:";
+        cout<<"A: Registrar un nuevo producto\n";
+        cout<<"B: Listar los productos registrados\n";
+        cout<<"Seleccione una opcion: ";
+        cin>>opcion;
+
+        switch (opcion) 
+		{
+            case 'A':
+            case 'a':
+                registrarProducto();
+                break;
+                case 'B':
+            case 'b':
+                listarProductos();
+                break;
+                 }
+    } while(opcion !='S'&&opcion!='s');
+
+    return 0;
+}
+
